18ac44177b56e41fc4387c28fcf38b6784553362
LalisaThaip <lizs.xrndr@gmail.com>
2025-08-17 12:05:49 +0930

    created Calculator interface

--- assignment1/src/Calculator.java
+++ assignment1/src/Calculator.java
@@ -1,2 +1,23 @@
-public interface Calculator {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+// method abstraction for remote calculator
+public interface Calculator extends Remote{
+    // get value from client
+    int getValue() throws RemoteException;
+
+    // push value from client onto the stack
+    void pushValue() throws RemoteException;
+
+    // get the operation from the client
+    void pushOperation(String operation) throws RemoteException;
+
+    // popping the value from stack
+    int popValue() throws RemoteException;
+
+    // boolean to check if the stack is empty
+    boolean isEmpty() throws RemoteException;
+
+    // delay the pop operation
+    float delayPop(int millis) throws RemoteException;
 }
