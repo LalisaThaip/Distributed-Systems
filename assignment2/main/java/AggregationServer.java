2e7f3b522ac51fdbfb2ed0d20c84abf6e3fc491f
LalisaThaip <lizs.xrndr@gmail.com>
2025-09-13 11:30:08 +0930

    added lamportclock imp

--- assignment2/main/java/AggregationServer.java
+++ assignment2/main/java/AggregationServer.java
@@ -1,21 +1,25 @@
-/*AggregationServer manages HTTP requests for weather data, maintains a persistent store, nd enforces lamport clock synchronisation. 
- *  Handles concurrent GET/PUT requests, data expiry, and crash recovery. 
- */
 import java.io.*;
 import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
-
+/** 
+ * AggregationServer manages HTTP requests for weather data, 
+ * maintains a persistent store, and enforces lamport clock synchronisation. 
+ * Handles concurrent GET/PUT requests, data expiry, and crash recovery. 
+ */
 
 public class AggregationServer implements IAggregationServer  {
-
     private static final int DEFAULT_PORT = 4567;
+    private static final int THREAD_POOL_SIZE = 10;
     private static final String DATA_FILE = "weather_data.json";
     private static final String LOG_FILE = "transaction.log";
     private static final int EXPIRY_TIME_MS = 30000; // 30 seconds
     private static final int EXPIRY_CHECK_INTERVAL_MS = 10000; // 10 seconds
-    private static long lamportClock = 0;
+    private final IWeatherDataStore dataStore;
+    private final ILamportClock lamportClock;
+
     // Data structures for storing weather data and last contact times
     // private static ConcurrentHashMap<String, WeatherData> weatherStore = new ConcurrentHashMap
 
